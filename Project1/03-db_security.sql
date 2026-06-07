13a1ee7d3f0baaf4cdb365816b32d709dfa8c195
Dallas Taylor <dallas.taylor96@outlook.com>
2026-06-07 12:17:15 -0500

    Changes name of Project1 folder and files

--- Project1/03-db_security.sql
+++
@@ -1,9 +0,0 @@
-/*
-Step 3 - Secure the Database
-
-Use this template file as the starting point to apply roles and users to your
-database and determine what they can and can't do.
-
-This is optional in training runs, but honestly? I say go nuts, build your
-database like it's going to run in production tomorrow. That's how you learn!
-*/
